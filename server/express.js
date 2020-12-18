fc10b57ebbc5a3f66e5ff9f522f71b883b6af7f7
Andres Tijera <6646584+android094x@users.noreply.github.com>
2020-12-17 21:13:11 -0500

    Adding Client and Basic Server-side Rendering

--- server/express.js
+++ server/express.js
@@ -6,6 +6,14 @@ import cors from "cors";
 import helmet from "helmet";
 import path from "path";
 
+// Modules for Server-side rendering
+import React from "react";
+import ReactDOMServer from "react-dom/server";
+import StaticRouter from "react-router-dom/StaticRouter";
+import MainRouter from "../client/MainRouter";
+import { ServerStyleSheets, ThemeProvider } from "@material-ui/styles";
+import theme from "../client/theme";
+
 import Template from "../template";
 import userRoutes from "./routes/user.routes";
 import authRoutes from "./routes/auth.routes";
@@ -33,9 +41,35 @@ app.use(cors());
 app.use("/", userRoutes);
 app.use("/", authRoutes);
 
-app.get("/", (req, res) => {
-  res.status(200).send(Template());
+app.get("*", (req, res) => {
+  //Generating CSS and markups
+  const sheets = new ServerStyleSheets();
+  const context = {};
+  const markup = ReactDOMServer.renderToString(
+    sheets.collect(
+      <StaticRouter location={req.url} context={context}>
+        <ThemeProvider theme={theme}>
+          <MainRouter />
+        </ThemeProvider>
+      </StaticRouter>
+    )
+  );
+
+  if (context.url) {
+    return res.redirect(303, context.url);
+  }
+  const css = sheets.toString();
+  res.status(200).send(
+    Template({
+      markup: markup,
+      css: css,
+    })
+  );
 });
+
+// app.get("/", (req, res) => {
+//   res.status(200).send(Template());
+// });
 
 // Catch unauthorised errors
 app.use((err, req, res, next) => {
